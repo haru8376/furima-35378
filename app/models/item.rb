2454ce677d88163eedfb154b5d4898bcab6e27e6
haru8376 <harunatakarada@gmail.com>
2021-07-02 16:35:17 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,22 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :shipping_date
 
   with_options presence: true do
     validates  :name,              length: { maximum: 40 }
     validates  :description,       length: { maximum: 1000 }
     validates  :price,             format: { with: /\A[0-9]+\z/ }, numericality: { greater_than: 300, less_than: 9999999 }
-    validates  :category_id,          
-    validates  :condition_id,         
-    validates  :postage_id,           
-    validates  :prefecture_id,        
-    validates  :shipping_date_id,     
+    validates  :category_id,       numericality: { other_than: 0 }  
+    validates  :condition_id,      numericality: { other_than: 0 }
+    validates  :postage_id,        numericality: { other_than: 0 }
+    validates  :prefecture_id,     numericality: { other_than: 0 }
+    validates  :shipping_date_id,  numericality: { other_than: 0 }
     validates  :image,                
   end
 end
