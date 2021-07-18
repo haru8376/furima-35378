8052a8b53b5c763c929a80687ef064af92264b8a
haru8376 <harunatakarada@gmail.com>
2021-07-18 11:29:02 +0900

    Formオブジェクトで実装した場合の単体テスト

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -8,7 +8,7 @@ class OrderAddress
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
     validates :city
     validates :house_number
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is too short. is invalid. Input only number" }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid. Input only number" }
   end
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
   validates :phone_number, numericality: { only_integer: true }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   before do
     @user = FactoryBot.build(:user)
+
+    
   end
 
   describe '#create' do
